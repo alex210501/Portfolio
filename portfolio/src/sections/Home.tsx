2d812599c87871999448703260db4c63f2fd97a8
Alex <alejandrobrb210501@gmail.com>
2024-01-31 19:39:30 +0100

    Add text presentation and avatar picture

--- portfolio/src/sections/Home.tsx
+++ portfolio/src/sections/Home.tsx
@@ -1,6 +1,7 @@
 import { Grid } from "@mui/material";
 import styled from "styled-components";
 
+import appleMemoji from "../images/memoji-apple-laptop.png";
 import StyledSection from "../styles/StyledSection";
 
 const StyledContainer = styled.div`
@@ -14,13 +15,43 @@ const StyledContainer = styled.div`
     width: 100%;
 `;
 
+const StyledPresentation = styled.div`
+    text-align: left;
+`;
+
+const StyledPresentationText = styled.h1`
+    margin: 0;
+    font-size: 2em;
+`;
+
+const StyledPresentationSpecialization = styled.h2`
+    margin: 0;
+    font-size: 1em;
+`;
+
+function Presentation() {
+    return (
+        <StyledPresentation>
+            <StyledPresentationText>Hi,</StyledPresentationText>
+            <StyledPresentationText>My name is <strong>Alejandro</strong></StyledPresentationText>
+            <StyledPresentationText>Software Engineer @ Datadog</StyledPresentationText>
+            <StyledPresentationSpecialization>Embedded Systems | Distributed Systems | Backend</StyledPresentationSpecialization>
+        </StyledPresentation>
+    );
+}
+
+function AvatarImage() {
+    return (
+        <img src={appleMemoji} alt="AB" />
+    );
+}
 
 export default function Home() {
     return (
         <StyledSection>
             <StyledContainer>
-                <div>Left</div>
-                <div>Right</div>
+                <Presentation />
+                <AvatarImage />
             </StyledContainer>
         </StyledSection>
     );
